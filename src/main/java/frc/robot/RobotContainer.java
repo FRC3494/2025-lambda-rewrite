501d31f15dad2dfed9ba0ef3892990adf22dfc22
liujip0 <liujip2020@gmail.com>
2025-09-11 01:43:31 -0400

    remove unnecessary controller from RobotContainer

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -17,7 +17,6 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 import frc.robot.commands.DriveCommands;
 import frc.robot.subsystems.drive.Drive;
@@ -36,9 +35,6 @@ import frc.robot.subsystems.drive.ModuleIOSpark;
 public class RobotContainer {
   // Subsystems
   private final Drive drive;
-
-  // Controller
-  private final CommandXboxController controller = new CommandXboxController(0);
 
   // Dashboard inputs
   private final LoggedDashboardChooser<Command> autoChooser;
